4cd2bf2470ffe526fc0c52c14d6f8ddcc6f649fb
Nand <nikjk2004@gmail.com>
2024-10-18 21:42:58 +0300

    first commit

---
+++ src/test/java/ru/nand/rbpo2/Rbpo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nand.rbpo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rbpo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
